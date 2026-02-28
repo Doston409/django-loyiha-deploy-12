61bef9db89d7b109ffd13b0af7c9d0d4d6c7a5d1
Пользователь <dabdugapparov806@gmail.com>
2026-02-28 19:05:38 +0500

    nimADUR

--- config/settings.py
+++ config/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-aend*=v-&(=roj(d8*m*6d)ac!74)k$7l6ercsn$g%pm^bek*f
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['mexepe5731.pythonanywhere.com']
 
 
 # Application definition
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'dars1',
+    'dars2',
 ]
 
 MIDDLEWARE = [
@@ -54,7 +56,7 @@ ROOT_URLCONF = 'config.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR, ' templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
